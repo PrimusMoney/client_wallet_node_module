0f0cce435fd74ca44390d1e35dba1aa4b7f1f937
fbaud <fbaud@p2pventure.org>
2021-04-07 11:26:34 +0200

    version 0.20.13

--- imports/includes/modules/module.js
+++ imports/includes/modules/module.js
@@ -6,7 +6,7 @@
 class ClientModules {
 	constructor() {
 		this.name = 'clientmodules';
-		this.current_version = "0.20.12.2021.04.07";
+		this.current_version = "0.20.13.2021.04.07";
 		
 		this.global = null; // put by global on registration
 		this.isready = false;
